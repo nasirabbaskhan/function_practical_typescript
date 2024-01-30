b5094e7937f450dfca6646d44caad2b3a12a04da
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-01-30 00:29:11 -0800

    Arrow_function

--- 2_0_arrow_function.js
+++ 2_0_arrow_function.js
@@ -3,10 +3,10 @@ let sum1 = (x, y) => {
     let sum = x + y;
     return sum;
 };
-let sum2 = (x, y) => x + y;
+let sum3 = (x, y) => x + y;
 let output = sum1(20, 30);
 console.log(`The some of x amd y is : ${output}`);
-let output1 = sum2(20, 90);
+let output1 = sum3(20, 90);
 console.log(`The some of x amd y is : ${output1}`);
 let returnNumberOrString; // to initilize type of arrow function
 //  to define the original function
